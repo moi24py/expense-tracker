e502695ac914f318508a7863babdbb13e7864a15
Moira <moi24py@gmail.com>
2024-11-28 18:02:18 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import modules.app_funcs as a
+
+def main():
+    # welcome message
+    a.intro()
+    # ask the user what they want to do
+    a.view_or_insert()
+
+if __name__ == '__main__':
+    main()
