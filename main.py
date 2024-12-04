f45d64c3813576e5aee0b754dcc3352fc63fcb0f
Moira <moi24py@gmail.com>
2024-12-04 19:30:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,32 +7,33 @@ def main():
     option = a.view_or_insert()
     
     if option:
-        if option == 'A':
-            a.add_new_expense()
-        if option == 'B':
-            # Print all the expenses in expense_list.csv
-            print("List of expenses:\n")
-            a.print_expenses(a.search_expenses())
-            exit()
-        if option == 'C':
-            a.print_category_expenses()
-        if option == 'D':
-            a.print_expenses_matched_by_name()
-        if option == 'E':
-            a.search_and_delete_expense()
-        if option == 'F':
-            a.view_balance()
-        if option == 'G':
-            # Add an amount to the balance
-            a.add_sub_money()
-            exit()
-        if option == 'H':
-            # Subtract an amount from the balance
-            a.add_sub_money('sub')
-            exit()
-        if option == '0':
-            print("Bye!\n")
-            exit()
+        match option:
+            case 'A':
+                a.add_new_expense()
+            case 'B':
+                # Print all the expenses in expense_list.csv
+                print("List of expenses:\n")
+                a.print_expenses(a.search_expenses())
+                exit()
+            case 'C':
+                a.print_category_expenses()
+            case 'D':
+                a.print_expenses_matched_by_name()
+            case 'E':
+                a.search_and_delete_expense()
+            case 'F':
+                a.view_balance()
+            case 'G':
+                # Add an amount to the balance
+                a.add_sub_money()
+                exit()
+            case 'H':
+                # Subtract an amount from the balance
+                a.add_sub_money('sub')
+                exit()
+            case '0':
+                print("Bye!\n")
+                exit()
 
 if __name__ == '__main__':
     main()
